a95b4e7860753f7f088bfc94f84a4fc635108fa1
dummy-preview <91373388+dummy-preview@users.noreply.github.com>
2022-08-23 23:52:51 +0700

    update

--- js/engine.js
+++ js/engine.js
@@ -275,3 +275,17 @@ $.each(instance, function(key, value) {
 
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
+
+$('[open-modal]').on('click', function() {
+    var id = $(this).attr('open-modal');
+    $('.modal#' + id).addClass('active');
+});
+
+$('[close-modal]').on('click', function() {
+    $(this).parents('.modal').removeClass('active');
+});
+
+$('.modal').on('click', function(e) {
+    if (e.target !== this) { return };
+    $(this).removeClass('active');
+});
